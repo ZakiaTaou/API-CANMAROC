cce10c64d235687dac9d275bca861e92c2faeea0
Samir-ibourki <samiribourki7@gmail.com>
2025-11-28 15:01:27 +0100

    rwina

--- server.js
+++ server.js
@@ -6,26 +6,19 @@ import "./src/models/Team.js";
 import "./src/models/Player.js";
 import "./src/models/Match.js";
 
-
-
-
 import authRoutes from "./src/routes/authRoutes.js";
-import teamRoutes from "./src/routes/teamRoutes.js"
-
+import teamRoutes from "./src/routes/teamRoutes.js";
 
 const app = express();
 const port = 5000;
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use("/api/teams", teamRoutes)
+app.use("/api/teams", teamRoutes);
 
 sequelize
   .sync({ alter: true })
   .then(() => console.log("Database synced successfully!"))
   .catch((err) => console.log("Error DB:", err));
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
